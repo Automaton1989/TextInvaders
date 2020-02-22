e304ccba93843fd61dfaa85c5e60f07727da0901
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2020-02-21 19:00:26 -0800

    Add files via upload

---
+++ CursesUtils.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef CURSESUTILS_H_
+#define CURSESUTILS_H_
+
+#include <string>
+#include <curses.h>
+
+
+enum ArrowKeys
+{
+	AK_UP = KEY_UP,
+	AK_DOWN = KEY_DOWN,
+	AK_LEFT = KEY_LEFT,
+	AK_RIGHT = KEY_RIGHT
+};
+
+void InitializeCurses(bool nodelay);
+void ShutDownCurses();
+void ClearScreen();
+void RefreshScreen();
+int ScreenWidth();
+int ScreenHeight();
+int GetChar();
+void DrawCharacter(int xPos, int yPos, char aCharacter);
+void MoveCursor(int xPos, int yPos);
+void DrawSprite(int xPos, int yPos, const char* sprite[], int spriteHeight, int offset = 0);
+void DrawString(int xPos, int yPos, const std::string& string);
+
+#endif // CURSESUTILS_H_
+
